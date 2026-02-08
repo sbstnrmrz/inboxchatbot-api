adf8684b8cb879cf6daa2e0725610394eb13f64d
sbstnrmrz <ramicastasebas@gmail.com>
2026-02-08 00:47:18 -0400

    feat: added tenantId index to user schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,10 @@ import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 
+client.connect().then(() => {
+  client.db().collection('user').createIndex({ tenantId: 1 });
+});
+
 export const auth = betterAuth({
   basePath: '/auth',
   database: mongodbAdapter(client.db(), { client }),
